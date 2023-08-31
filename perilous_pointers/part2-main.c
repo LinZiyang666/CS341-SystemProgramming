f8309957fcb678cff1efd903f62de99f067fe057
Vlad Petru Nitu <vnitu2@illinois.edu>
2023-08-31 12:17:52 -0500

    Done first 3 parts of Part2, 4th Illinois WRONG

--- perilous_pointers/part2-main.c
+++ perilous_pointers/part2-main.c
@@ -12,6 +12,32 @@
  * part2-functions.c in order.)
  */
 int main() {
-    // your code here
+
+    first_step(81);
+
+    int i132 = 132;
+    second_step(&i132);
+
+    int **matrix = malloc(1 * sizeof(int*));
+    matrix[0] = malloc(1 * sizeof(int));
+    matrix[0][0] = 8942;
+    double_step(matrix);
+    free(matrix[0]);
+    matrix[0] = NULL;
+    free(matrix); 
+    matrix = NULL;
+
+    char *msg = malloc(10);
+    strcpy(msg, "VladP");
+    for (int i = 5; i < 9; ++i)
+      msg[i] = 1;
+    msg[9] = '\0';
+    free(msg);
+    msg = NULL;
+
+
+
+
+
     return 0;
 }
