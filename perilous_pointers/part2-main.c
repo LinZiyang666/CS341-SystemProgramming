41b6b65238150ed8c9935b10587ead1b6072edea
Vlad Petru Nitu <vnitu2@illinois.edu>
2023-08-31 20:16:25 -0500

    Finished part2 [45mins]

--- perilous_pointers/part2-main.c
+++ perilous_pointers/part2-main.c
@@ -13,31 +13,64 @@
  */
 int main() {
 
-    first_step(81);
+  first_step(81);
 
-    int i132 = 132;
-    second_step(&i132);
+  int i132 = 132;
+  second_step(&i132);
 
-    int **matrix = malloc(1 * sizeof(int*));
-    matrix[0] = malloc(1 * sizeof(int));
-    matrix[0][0] = 8942;
-    double_step(matrix);
-    free(matrix[0]);
-    matrix[0] = NULL;
-    free(matrix); 
-    matrix = NULL;
+  int **matrix = malloc(1 * sizeof(int *));
+  matrix[0] = malloc(1 * sizeof(int));
+  matrix[0][0] = 8942;
+  double_step(matrix);
+  free(matrix[0]);
+  matrix[0] = NULL;
+  free(matrix);
+  matrix = NULL;
 
-    char *msg = malloc(10);
-    strcpy(msg, "VladP");
-    for (int i = 5; i < 9; ++i)
-      msg[i] = 1;
-    msg[9] = '\0';
-    free(msg);
-    msg = NULL;
+  char *msg = malloc(9);
+  strcpy(msg, "VladP");
+  *(int *)(msg + 5) = 15;
+  strange_step(msg);
+  free(msg);
+  msg = NULL;
 
+  msg = malloc(4);
+  msg[3] = 0;
+  empty_step(msg);
+  free(msg);
+  msg = NULL;
 
+  msg = malloc(4);
+  msg[3] = 'u'; 
+  two_step(msg, msg); 
+  free(msg);
+  msg = NULL;
 
+  msg = "abcde"; 
+  three_step(msg, msg + 2, msg + 4); // "abcde", "cde" and "e" 
+  
+  msg = "\0aiq"; 
+  step_step_step(msg, msg, msg);
 
+  msg = malloc(4);
+  * (int *)(msg) = 5;
+  int b = 5;
+  it_may_be_odd(msg, b);
+  free(msg);
+  msg = NULL;
 
-    return 0;
+  msg = strdup("Course,CS241"); // after checking `strtok` man page BUGS section: strtok cannot be used on `const literals`
+  tok_step(msg);
+  free(msg);
+  msg = NULL;
+
+  msg = malloc(4); 
+  msg[0] = 1;
+  msg[1] = 2; 
+  msg[2] = msg[3] = '\0';
+  the_end(msg, msg);
+  free(msg); 
+  msg = NULL;
+
+  return 0;
 }
