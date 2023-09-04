c37eab1489ff855adcd3662b75655af43f3fa7c2
Vlad Petru Nitu <vnitu2@illinois.edu>
2023-09-04 17:41:05 -0500

    Initial progress MP2 vector.c until vetor_reserve

--- vector/vector.c
+++ vector/vector.c
@@ -15,7 +15,8 @@ const size_t INITIAL_CAPACITY = 8;
  */
 const size_t GROWTH_FACTOR = 2;
 
-struct vector {
+struct vector
+{
     /* The function callback for the user to define the way they want to copy
      * elements */
     copy_constructor_type copy_constructor;
@@ -72,14 +73,16 @@ struct vector {
  * We have provided get_new_capacity() to help make this less ambigious.
  */
 
-static size_t get_new_capacity(size_t target) {
+static size_t get_new_capacity(size_t target)
+{
     /**
      * This function works according to 'automatic reallocation'.
      * Start at 1 and keep multiplying by the GROWTH_FACTOR untl
      * you have exceeded or met your target capacity.
      */
     size_t new_capacity = 1;
-    while (new_capacity < target) {
+    while (new_capacity < target)
+    {
         new_capacity *= GROWTH_FACTOR;
     }
     return new_capacity;
@@ -87,157 +90,246 @@ static size_t get_new_capacity(size_t target) {
 
 vector *vector_create(copy_constructor_type copy_constructor,
                       destructor_type destructor,
-                      default_constructor_type default_constructor) {
+                      default_constructor_type default_constructor)
+{
     // your code here
+
+    vector *v = (vector *)malloc(sizeof(vector));
+    v->array = (void **)malloc(sizeof(void *) * INITIAL_CAPACITY);
+    v->capacity = INITIAL_CAPACITY;
+    v->copy_constructor = copy_constructor ? copy_constructor : shallow_copy_constructor;
+    v->default_constructor = default_constructor ? default_constructor : shallow_default_constructor;
+    v->destructor = destructor ? destructor : shallow_destructor;
+    v->size = 0;
+
+    return v;
+
     // Casting to void to remove complier error. Remove this line when you are
     // ready.
-    (void)INITIAL_CAPACITY;
-    (void)get_new_capacity;
-    return NULL;
+    // return NULL;
 }
 
-void vector_destroy(vector *this) {
+void vector_destroy(vector *this)
+{ // Destroys all container elements
+
     assert(this);
-    // your code here
+
+    for (size_t i = 0; i < this->size; ++i)
+        this->destructor(this->array[i]); // By: calling on the `user provided destructor` for every element
+
+    // and deallocates all the storage capacity allocated by the 'vector' constructor.
+    free(this->array);
+    free(this);
+    this = NULL;
 }
 
-void **vector_begin(vector *this) {
+void **vector_begin(vector *this)
+{
     return this->array + 0;
 }
 
-void **vector_end(vector *this) {
+void **vector_end(vector *this)
+{
     return this->array + this->size;
 }
 
-size_t vector_size(vector *this) {
+size_t vector_size(vector *this)
+{
     assert(this);
     // your code here
-    return 0;
+    return this->size;
 }
 
-void vector_resize(vector *this, size_t n) {
+void vector_resize(vector *this, size_t n)
+{
+
     assert(this);
     // your code here
+    if (n < this->size)
+    {
+        for (size_t i = n; i < this->size; ++i)
+            this->destructor(this->array[i]);
+        this->size = n;
+    }
+    else if (n >= this->size)
+    {
+
+        // i.)
+        if (n <= this->capacity)
+        {
+
+            for (size_t i = this->size; i < n; ++i)
+                this->array[i] = this->default_constructor();
+
+            this->size = n;
+        }
+        else
+        { // ii.)
+
+            // this->capacity = get_new_capacity(n); // `automatic reallocation` -> use `get_new_capacity`
+            // this->array = (void **)realloc(this->array, this->capacity * sizeof(void *));
+
+            vector_reserve(this, n);
+
+            for (size_t i = this->size; i < n; ++i)
+                this->array[i] = this->default_constructor();
+
+            this->size = n;
+        }
+    }
 }
 
-size_t vector_capacity(vector *this) {
+size_t vector_capacity(vector *this)
+{
     assert(this);
     // your code here
-    return 0;
+    return this -> capacity;
 }
 
-bool vector_empty(vector *this) {
+bool vector_empty(vector *this)
+{
     assert(this);
     // your code here
-    return true;
+    return this -> size == 0;
 }
 
-void vector_reserve(vector *this, size_t n) {
+    /*
+    NOTE:
+    The capacity of a vector can be explicitly altered by calling vector_reserve().
+    */
+void vector_reserve(vector *this, size_t n)
+{
     assert(this);
     // your code here
+    if (n > this -> capacity) {
+        this -> capacity = get_new_capacity(n);
+        this -> array = (void **) realloc(this -> array, this -> capacity * sizeof (void *));
+    }
 }
 
-void **vector_at(vector *this, size_t position) {
+void **vector_at(vector *this, size_t position)
+{
     assert(this);
     // your code here
     return NULL;
 }
 
-void vector_set(vector *this, size_t position, void *element) {
+void vector_set(vector *this, size_t position, void *element)
+{
     assert(this);
     // your code here
 }
 
-void *vector_get(vector *this, size_t position) {
-    assert(this);
-    // your code here
-    return NULL;
-}
-
-void **vector_front(vector *this) {
+void *vector_get(vector *this, size_t position)
+{
     assert(this);
     // your code here
     return NULL;
 }
 
-void **vector_back(vector *this) {
+void **vector_front(vector *this)
+{
+    assert(this);
     // your code here
     return NULL;
 }
 
-void vector_push_back(vector *this, void *element) {
+void **vector_back(vector *this)
+{
+    // your code here
+    return NULL;
+}
+
+void vector_push_back(vector *this, void *element)
+{
     assert(this);
     // your code here
 }
 
-void vector_pop_back(vector *this) {
+void vector_pop_back(vector *this)
+{
     assert(this);
     // your code here
 }
 
-void vector_insert(vector *this, size_t position, void *element) {
+void vector_insert(vector *this, size_t position, void *element)
+{
     assert(this);
     // your code here
 }
 
-void vector_erase(vector *this, size_t position) {
+void vector_erase(vector *this, size_t position)
+{
     assert(this);
     assert(position < vector_size(this));
     // your code here
 }
 
-void vector_clear(vector *this) {
+void vector_clear(vector *this)
+{
     // your code here
 }
 
 // The following is code generated:
-vector *shallow_vector_create() {
+vector *shallow_vector_create()
+{
     return vector_create(shallow_copy_constructor, shallow_destructor,
                          shallow_default_constructor);
 }
-vector *string_vector_create() {
+vector *string_vector_create()
+{
     return vector_create(string_copy_constructor, string_destructor,
                          string_default_constructor);
 }
-vector *char_vector_create() {
+vector *char_vector_create()
+{
     return vector_create(char_copy_constructor, char_destructor,
                          char_default_constructor);
 }
-vector *double_vector_create() {
+vector *double_vector_create()
+{
     return vector_create(double_copy_constructor, double_destructor,
                          double_default_constructor);
 }
-vector *float_vector_create() {
+vector *float_vector_create()
+{
     return vector_create(float_copy_constructor, float_destructor,
                          float_default_constructor);
 }
-vector *int_vector_create() {
+vector *int_vector_create()
+{
     return vector_create(int_copy_constructor, int_destructor,
                          int_default_constructor);
 }
-vector *long_vector_create() {
+vector *long_vector_create()
+{
     return vector_create(long_copy_constructor, long_destructor,
                          long_default_constructor);
 }
-vector *short_vector_create() {
+vector *short_vector_create()
+{
     return vector_create(short_copy_constructor, short_destructor,
                          short_default_constructor);
 }
-vector *unsigned_char_vector_create() {
+vector *unsigned_char_vector_create()
+{
     return vector_create(unsigned_char_copy_constructor,
                          unsigned_char_destructor,
                          unsigned_char_default_constructor);
 }
-vector *unsigned_int_vector_create() {
+vector *unsigned_int_vector_create()
+{
     return vector_create(unsigned_int_copy_constructor, unsigned_int_destructor,
                          unsigned_int_default_constructor);
 }
-vector *unsigned_long_vector_create() {
+vector *unsigned_long_vector_create()
+{
     return vector_create(unsigned_long_copy_constructor,
                          unsigned_long_destructor,
                          unsigned_long_default_constructor);
 }
-vector *unsigned_short_vector_create() {
+vector *unsigned_short_vector_create()
+{
     return vector_create(unsigned_short_copy_constructor,
                          unsigned_short_destructor,
                          unsigned_short_default_constructor);
