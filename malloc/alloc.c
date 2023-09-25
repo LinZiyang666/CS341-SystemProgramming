1eb9510b62fbcb43e6047c8c4a6aab3cd7cbe504
Vlad Petru Nitu <vnitu2@illinois.edu>
2023-09-24 23:48:28 -0500

    Malloc impl

--- malloc/alloc.c
+++ malloc/alloc.c
@@ -8,27 +8,38 @@
 #include <unistd.h>
 
 struct node {
-    char is_free; // 1, if free, 0 else
-    size_t size; // size of node
-    struct node *prev;  // pointer to prev node
-    struct node *next;  // pointer to next node
+  char is_free;      // 1, if free, 0 else
+  size_t size;       // size of node
+  struct node *prev; // pointer to prev node
+  struct node *next; // pointer to next node
 };
 
 typedef struct node node_t;
 
 // Try implementing a simpler variant of a binary Buddy Allocator
 
-static size_t buddy_size[16] = {(1 << 3), (1 << 4), (1 << 5), (1 << 6), (1 << 7), (1 << 8), (1 << 9), (1 << 10), (1 << 11), (1 << 12), (1 << 13), (1 << 14), (1 << 15), (1 << 16), (1 << 17),  (1 <<18)}; // buddy_size[i] = head of free list allocator associated w/ size 2^(i+3), i in [0, 16); allocate all free blocks w/ sz <= 2 ^i in buddy_size[i]; if sz > (1 << 18), allocate to buddy_size[15] - last cell
+static size_t FRAGMENT = 4; // TODO: Tweak this param. afterwards
 
-static node_t *suballocators_h[16]; // suballocators_h[i] = head of the suballocator list for nodes w/ size <= 2^i; check `buddy-size` for more info
+static size_t
+    buddy_size[16] = {(1 << 3),  (1 << 4),  (1 << 5),  (1 << 6),  (1 << 7),
+                      (1 << 8),  (1 << 9),  (1 << 10), (1 << 11), (1 << 12),
+                      (1 << 13), (1 << 14), (1 << 15), (1 << 16), (1 << 17),
+                      (1 << 18)}; // buddy_size[i] = head of free list allocator
+                                  // associated w/ size 2^(i+3), i in [0, 16);
+                                  // allocate all free blocks w/ sz <= 2 ^i in
+                                  // buddy_size[i]; if sz > (1 << 18), allocate
+                                  // to buddy_size[15] - last cell
 
+static node_t *suballocators_h[16]; // suballocators_h[i] = head of the
+                                    // suballocator list for nodes w/ size <=
+                                    // 2^i; check `buddy-size` for more info
 
-void *allocate_node(size_t size);
-size_t get_allocator(size_t size); 
-void *best_fit(node_t *node, size_t allocator);
+size_t get_allocator(size_t size);
+node_t *allocate_node(size_t size);
+node_t *best_fit(size_t size, size_t allocator);
 void remove_from_free_list(node_t *node, size_t allocator);
-void split_node(node_t *node, size_t size);
-
+void split_node(node_t *node, size_t new_size);
+void insert_front(node_t *node, size_t allocator);
 
 /**
  * Allocate space for array in memory
@@ -54,14 +65,15 @@ void split_node(node_t *node, size_t size);
  * @see http://www.cplusplus.com/reference/clibrary/cstdlib/calloc/
  */
 void *calloc(size_t num, size_t size) {
-    // implement calloc!
-    size_t nbytes = num * size; 
-    char *ptr = malloc(nbytes);
-    if (ptr == NULL) return NULL;
-    else {
-        memset(ptr, 0, nbytes); // zero out the bytes;
-        return ptr;
-    }
+  // implement calloc!
+  size_t nbytes = num * size;
+  char *ptr = malloc(nbytes);
+  if (ptr == NULL)
+    return NULL;
+  else {
+    memset(ptr, 0, nbytes); // zero out the bytes;
+    return ptr;
+  }
 }
 
 /**
@@ -86,30 +98,31 @@ void *calloc(size_t num, size_t size) {
  * @see http://www.cplusplus.com/reference/clibrary/cstdlib/malloc/
  */
 void *malloc(size_t size) {
-    // implement malloc!
-    
-    if (size <= 0) // don't allocate - @see http://www.cplusplus.com/reference/clibrary/cstdlib/malloc/
-        return NULL;
-    size_t allocator = get_allocator(size);
+  // implement malloc!
 
-    if (suballocators_h[allocator] == NULL) { // no free node that we can reuse => use `sbrk`
-        void *ptr = allocate_node(size);
-        if (ptr == (void*) -1)
-            return NULL;
-        return ptr;
+  if (size <= 0) // don't allocate - @see
+                 // http://www.cplusplus.com/reference/clibrary/cstdlib/malloc/
+    return NULL;
+  size_t allocator = get_allocator(size);
+
+  if (suballocators_h[allocator] ==
+      NULL) { // no free node that we can reuse => use `sbrk`
+    node_t *ptr = allocate_node(size);
+    if (ptr == (void *)-1)
+      return NULL;
+    return ptr;
+  } else {
+    node_t *ptr = best_fit(size, allocator);
+    if (!ptr) {
+      ptr = allocate_node(size);
+      return ptr;
+    } else {
+      remove_from_free_list(ptr, allocator);
+      ptr->is_free = 0; // mark as taken
+      split_node(ptr, size);
+      return ((node_t *)ptr + 1); // Return the (!) user-accessible memory (!)
     }
-    else {
-        void *ptr = best_fit(size, allocator); 
-        if (!ptr)
-            ptr = allocate_node(size);
-        else 
-        {
-            remove_from_free_list(ptr, allocator);
-            ptr->free = 0; // mark as taken
-            split_node(ptr, size);
-            return ((node_t *)ptr + 1); // Return the (!) user-accessible memory (!) 
-        }
-    }
+  }
 }
 
 /**
@@ -129,7 +142,7 @@ void *malloc(size_t size) {
  *    passed as argument, no action occurs.
  */
 void free(void *ptr) {
-    // implement free!
+  // implement free!
 }
 
 /**
@@ -178,6 +191,106 @@ void free(void *ptr) {
  * @see http://www.cplusplus.com/reference/clibrary/cstdlib/realloc/
  */
 void *realloc(void *ptr, size_t size) {
-    // implement realloc!
+  // implement realloc!
+  return NULL;
+}
+
+// ------------------ HELPER FUNCTIONS ------------------
+
+/*
+  Get the index in the buddy_size C-array, which tells us: what free list (i.e:
+  suballocators) to use for the free node w/ this size
+*/
+size_t get_allocator(size_t size) {
+
+  for (size_t allocator = 0; allocator < 16; ++allocator)
+    if (size <= buddy_size[allocator]) // found!
+      return allocator;
+
+  return 15; // TODO: Later improve the policy to not allocate all free nodes w/
+             // size > (1 << 16) to last allocator
+}
+
+/*
+ sbrk() to increase the heap size
+*/
+node_t *allocate_node(size_t size) {
+  node_t *node = sbrk(sizeof(node_t) + size);
+  if (node == (void *)-1)
     return NULL;
+  node->size = size;
+  node->is_free = 0; // taken
+  node->prev = node->next = NULL;
+  return node;
+}
+
+/*
+ * Search for the best_fit in the suballocator specific to size `size`
+ * best_fit = the smallest size node that has enough capacity (node->size >=
+ * size) to allow the current node to fit (i.e.: has at least size `size`) In
+ * case of tie -> choose the left-most (first one found) one If no node found ->
+ * ret. NULL
+ */
+node_t *best_fit(size_t size, size_t allocator) {
+  node_t *node = NULL;
+
+  node_t *suballocator_h = suballocators_h[allocator];
+  for (node_t *walk = suballocator_h; walk != NULL; walk = walk->next) {
+    if (size <= walk->size) { // candidate
+      if (!node || node->size > size)
+        node = walk;
+    }
+  }
+  return node;
+}
+
+void remove_from_free_list(node_t *node, size_t allocator) {
+  node_t *prev_node = node->prev;
+  node_t *next_node = node->next;
+
+  if (prev_node == NULL) // it was the head
+  {
+    suballocators_h[allocator] = NULL;
+  } else {
+    prev_node->next = next_node;
+    if (next_node)
+      next_node->prev = prev_node;
+  }
+
+  // Clean-up links (redundant, but great practice)
+  node->prev = node->next = NULL;
+}
+
+/*
+* Split the `node` into -> node with `new_size` of data being used (in order to reduce the internal fragmentation) & node `created` that is marked and free and added to the associated suballocator's list
+*/
+void split_node(node_t *node, size_t new_size) { 
+    
+    if (node->size >= new_size + sizeof(node_t) + FRAGMENT)  // Only split if we exceed the FRAGMENT treshold, to: not external fragment our memory
+    { // We are ensured to write in a memory owned by US
+        node_t *created = (node_t *) ((char *)(node + 1) + node -> size);
+        created->size = node->size - (new_size + sizeof(node_t));
+        created->is_free = 1;
+
+        node->size = new_size;
+
+        size_t allocator = get_allocator(created->size);
+        insert_front(created, allocator);
+    }
+}
+
+/*
+* Insert `node` to the front of the suballocator indexed at `allocator` 's list
+* If the list is EMPTY -> make `node` the head of the list
+*/
+void insert_front(node_t *node, size_t allocator) { 
+    node_t *suballocator_h = suballocators_h[allocator]; 
+    if (suballocator_h == NULL) {
+        suballocators_h[allocator] = node;
+    }
+
+    node -> prev = NULL;
+    node -> next = suballocator_h; 
+    suballocator_h -> prev = node; 
+    suballocators_h[allocator] = node;
 }
