abedc6a489518ce9ed4978f0fd2fd65dbfe04cce
Vlad Petru Nitu <vnitu2@illinois.edu>
2023-09-28 20:31:44 -0500

    Semamore impl. & 3 tests

--- critical_concurrency/semamore.c
+++ critical_concurrency/semamore.c
@@ -4,6 +4,7 @@
  */
 
 #include "semamore.h"
+#include <pthread.h>
 
 /**
  * Initializes the Semamore. Important: the struct is assumed to have been
@@ -14,7 +15,11 @@
  *
  */
 void semm_init(Semamore *s, int value, int max_val) {
-    /* Your code here */
+  /* Your code here */
+  s->value = value;
+  s->max_val = max_val;
+  pthread_mutex_init(&s->m, NULL);
+  pthread_cond_init(&s->cv, NULL);
 }
 
 /**
@@ -22,7 +27,16 @@ void semm_init(Semamore *s, int value, int max_val) {
  *  Otherwise, should decrement the value.
  */
 void semm_wait(Semamore *s) {
-    /* Your code here */
+  /* Your code here */
+  pthread_mutex_lock(&s->m);
+  /* Critical Section Starts */
+  while (s->value == 0) { // block
+    pthread_cond_wait(&s->cv, &s->m);
+  }
+  s->value--;
+  pthread_cond_broadcast(&s->cv);
+  /* Critical Section Ends */
+  pthread_mutex_unlock(&s->m);
 }
 
 /**
@@ -30,8 +44,17 @@ void semm_wait(Semamore *s) {
  * max_value.
  *  Otherwise, should increment the value.
  */
-void semm_post(Semamore *s) {
-    /* Your code here */
+void semm_post(Semamore *s) { /* Your code here */
+  pthread_mutex_lock(&s->m);
+  /* Critical Section Starts */
+  while (s->value == s->max_val) { // block
+    pthread_cond_wait(&s->cv, &s->m);
+  }
+  s->value++;
+  pthread_cond_broadcast(&s->cv);   /* (probably) a sleeping thread is waiting to acquire the lock, so it will also have to wait until we call unlock*/ 
+
+  /* Critical Section Ends */
+  pthread_mutex_unlock(&s->m);
 }
 
 /**
@@ -39,6 +62,7 @@ void semm_post(Semamore *s) {
  * struct.
  * The actual Semamore struct must be freed by the user.
  */
-void semm_destroy(Semamore *s) {
-    /* Your code here */
+void semm_destroy(Semamore *s) { /* Your code here */
+    pthread_mutex_destroy(&s->m);
+    pthread_cond_destroy(&s->cv);
 }
--- critical_concurrency/semamore_test.c
+++ critical_concurrency/semamore_test.c
@@ -2,6 +2,7 @@
  * critical_concurrency
  * CS 341 - Fall 2023
  */
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -9,7 +10,96 @@
 
 #include "semamore.h"
 
+// ************** HELPER FUNCTIONS **************  // 
+
+void *push(void *arg) {
+  Semamore *s = (Semamore *)arg;
+  semm_post(s);
+  printf("I pushed :D\n");
+  return NULL;
+}
+
+void *pull(void *arg) {
+  Semamore *s = (Semamore *)arg;
+  semm_wait(s);
+  printf("I pulled :D\n");
+  return NULL;
+}
+
+// ************** HELPER FUNCTIONS **************  // 
+
+void t1() {
+  /* A woken thread must acquire the lock, so it will also have to wait until we
+   * call unlock*/
+
+  Semamore *s = malloc(sizeof(Semamore));
+  semm_init(s, 5, 10);
+
+  printf("s->value: %d\n", s->value);
+  printf("s->max_val: %d\n", s->max_val);
+
+  semm_post(s);
+  printf("Expected s->value: 6, and got: %d\n", s->value);
+  printf("s->max_val should not change; Expected: 10, and got: %d\n",
+         s->max_val);
+
+  for (int i = 0; i < 3; ++i)
+    semm_wait(s);
+
+  printf("Expected s->value: 3, and got: %d\n", s->value);
+
+
+  printf("I will destroy the sem now\n");
+  semm_destroy(s);
+  free(s); // Don't forget to free up the semaphore mem
+  printf("---------------\n");
+}
+
+void wait_all_block() {
+  Semamore *s = malloc(sizeof(Semamore));
+  semm_init(s, 5, 10);
+  for (int i = 0; i < 5; ++i)
+    semm_wait(s);
+
+  printf("Expected s->value: 0, and got: %d\n", s->value);
+
+  pthread_t tid1, tid2;
+  pthread_create(&tid1, NULL, pull, (void *)s);
+  pthread_create(&tid2, NULL, push, (void *)s);
+
+  pthread_join(tid1, NULL);
+  pthread_join(tid2, NULL);
+
+  semm_destroy(s);
+  free(s); // Don't forget to free up the semaphore mem
+  printf("---------------\n");
+}
+
+void post_all_block() {
+  Semamore *s = malloc(sizeof(Semamore));
+  semm_init(s, 5, 10);
+  for (int i = 0; i < 5; ++i)
+    semm_post(s);
+
+  printf("Expected s->value: 10 == max->value: 10, and got: %d == %d\n", s->value, s->max_val);
+
+  pthread_t tid1, tid2;
+  pthread_create(&tid2, NULL, push, (void *)s);
+  pthread_create(&tid1, NULL, pull, (void *)s);
+
+  pthread_join(tid1, NULL);
+  pthread_join(tid2, NULL);
+
+  semm_destroy(s);
+  free(s); // Don't forget to free up the semaphore mem
+  printf("---------------\n");
+}
+
 int main(int argc, char **argv) {
-    printf("Please write tests in semamore_tests.c\n");
-    return 0;
+  // printf("Please write tests in semamore_tests.c\n");
+  t1();
+  wait_all_block();
+  post_all_block();
+
+  return 0;
 }
