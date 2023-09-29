1b837a8df9ba420cd3666ef97f572cd4a7c14702
Vlad Petru Nitu <vnitu2@illinois.edu>
2023-09-28 19:56:44 -0500

    solved rendesvouz

--- critical_concurrency/rendezvous.c
+++ critical_concurrency/rendezvous.c
@@ -13,12 +13,17 @@ static char *quote_A;
 static char *quote_B;
 
 // Create some Semaphores!
+//
+static sem_t s1;
+static sem_t s2;
 
 static void *modifyB_printA();
 static void *modifyA_printB();
 
 int main(int argc, char **argv) {
     // Initialize your semaphores
+    sem_init(&s1, 0, 0);
+    sem_init(&s2, 0, 0);
 
     quote_A = strdup("J!epo(u!lopx!ipx!nboz!pg!zpv!ibwf!fwfs!nfu!Ejkltusb-!cvu!"
                      "zpv!qspcbcmz!lopx!uibu!bssphbodf!jo!dpnqvufs!tdjfodf!jt!"
@@ -38,28 +43,41 @@ int main(int argc, char **argv) {
     free(quote_A);
 
     // Destroy your semaphores
+    sem_destroy(&s1);
+    sem_destroy(&s2);
 
     return 0;
 }
 
 static void *modifyA_printB() {
+
     int i = 0;
     while (quote_A[i]) {
         usleep(rand() & 15); // randomized slowdowns
         quote_A[i++]--;
     }
 
+    sem_post(&s1);
+    sem_wait(&s2);
+
     printf("quote_B:\t%s\n", quote_B);
+
+
     return NULL;
 }
 
 static void *modifyB_printA() {
+
     int i = 0;
     while (quote_B[i]) {
         usleep(rand() & 100); // randomized slowdowns
         quote_B[i++]--;
     }
 
+    sem_post(&s2);
+    sem_wait(&s1);
+
     printf("quote_A:\t%s\n", quote_A);
+
     return NULL;
 }
