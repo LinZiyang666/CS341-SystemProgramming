b7b8e49457bec3b40588312cbda568be883003da
Vlad Petru Nitu <vnitu2@illinois.edu>
2023-09-28 22:57:41 -0500

    queue.c impl & 4 tests for it

--- critical_concurrency/queue.c
+++ critical_concurrency/queue.c
@@ -11,42 +11,106 @@
  * This queue is implemented with a linked list of queue_nodes.
  */
 typedef struct queue_node {
-    void *data;
-    struct queue_node *next;
+  void *data;
+  struct queue_node *next;
 } queue_node;
 
 struct queue {
-    /* queue_node pointers to the head and tail of the queue */
-    queue_node *head, *tail;
+  /* queue_node pointers to the head and tail of the queue */
+  queue_node *head, *tail;
 
-    /* The number of elements in the queue */
-    ssize_t size;
+  /* The number of elements in the queue */
+  ssize_t size;
 
-    /**
-     * The maximum number of elements the queue can hold.
-     * max_size is non-positive if the queue does not have a max size.
-     */
-    ssize_t max_size;
+  /**
+   * The maximum number of elements the queue can hold.
+   * max_size is non-positive if the queue does not have a max size.
+   */
+  ssize_t max_size;
 
-    /* Mutex and Condition Variable for thread-safety */
-    pthread_cond_t cv;
-    pthread_mutex_t m;
+  /* Mutex and Condition Variable for thread-safety */
+  pthread_cond_t cv;
+  pthread_mutex_t m;
 };
 
 queue *queue_create(ssize_t max_size) {
-    /* Your code here */
-    return NULL;
+  /* Your code here */
+  queue *q = malloc(sizeof(queue));
+  q->head = q->tail = NULL;
+  q->max_size = max_size;
+  q->size = 0;
+  pthread_mutex_init(&q->m, NULL);
+  pthread_cond_init(&q->cv, NULL);
+  return q;
 }
 
 void queue_destroy(queue *this) {
-    /* Your code here */
+  /* Your code here */
+  pthread_cond_destroy(&this->cv);
+  pthread_mutex_destroy(&this->m);
+  queue_node *walk = this->head;
+  while (walk != this->tail) {
+    // !!!!!! free(walk->data); // WRONG -> deallocate all the storage capacaity allocated by the queue, so only the `queue_node*`, the `void *data` was passed as arg, we used the one allocated by the caller
+    queue_node *prev = walk;
+    walk = walk->next;
+    prev->next = NULL;
+    free(prev);
+    prev = NULL;
+  }
+
+  free(walk); // == this->tail; (!) make sure to not double-free
+  free(this);
 }
 
 void queue_push(queue *this, void *data) {
-    /* Your code here */
+  /* Your code here */
+  pthread_mutex_lock(&this->m);
+
+  while (
+      this->size ==
+      this->max_size) // Blocks if the queue is full (defined by it's max_size).
+                      // ; if max_size < 0, while cond is always false
+    pthread_cond_wait(&this->cv, &this->m);
+
+  // Enque to end (tail)
+  queue_node *node = malloc(sizeof(queue_node));
+  node->data = data;
+  node->next = NULL;
+
+  if (this->tail) {
+    this->tail->next = node;
+    this->tail = node;
+  } else {
+    this->head = this->tail = node;
+  }
+
+  this->size++;
+  pthread_cond_broadcast(&this->cv);
+  pthread_mutex_unlock(&this->m);
 }
 
 void *queue_pull(queue *this) {
-    /* Your code here */
-    return NULL;
+  /* Your code here */
+
+  pthread_mutex_lock(&this->m);
+  queue_node *node = NULL;
+
+  while (this->size == 0) // Blocks if the queue is empty.
+    pthread_cond_wait(&this->cv, &this->m);
+
+  void *data = this->head->data;
+  node = this->head;
+  this->head = this->head->next;  
+  free(node);
+  node = NULL;
+
+  this->size--;
+
+  if (this->size == 0)
+    this->head = this->tail = NULL;
+
+  pthread_cond_broadcast(&this->cv);
+  pthread_mutex_unlock(&this->m);
+
+  return data;  // TODO: clarify if we return data or node
 }
--- critical_concurrency/queue_test.c
+++ critical_concurrency/queue_test.c
@@ -10,11 +10,102 @@
 
 #include "queue.h"
 
+
+/* ----------------- HELPER METHODS ------------------- */ 
+
+void *push(void *arg) {
+    queue *q = (queue *) arg;
+    queue_push(q, (void *)2);
+    return NULL;
+}
+
+void *pull(void *arg) {
+    queue *q = (queue *) arg;
+    printf("q.top() = %ld\n", (size_t) queue_pull(q)); // try to pull before pushing
+    return NULL;
+}
+
+void t1() {
+    queue *q = queue_create(10);
+    queue_push(q, (void *)1);
+    printf("q.top() = %ld\n", (size_t) queue_pull(q));
+    queue_push(q, (void *)2);
+    printf("q.top() = %ld\n", (size_t) queue_pull(q));
+    queue_push(q, (void *)3);
+    printf("q.top() = %ld\n", (size_t) queue_pull(q));
+
+    queue_push(q, (void *) 5);
+    queue_push(q, (void *) 6);
+    queue_push(q, (void *) 7);
+    printf("q.top() = %ld\n", (size_t) queue_pull(q));
+
+    queue_destroy(q);
+}
+
+void block_pull_empty() {
+    queue *q = queue_create(10);
+    queue_push(q, (void *)1);
+    printf("q.top() = %ld\n", (size_t) queue_pull(q));
+
+    pthread_t tid1, tid2;
+    pthread_create(&tid1, NULL, pull, (void *) q);
+    pthread_create(&tid2, NULL, push, (void *) q);
+
+    pthread_join(tid1, NULL);
+    pthread_join(tid2, NULL);
+
+    queue_destroy(q);
+}
+
+void block_push_max_size() {
+    queue *q = queue_create(3);
+    queue_push(q, (void *)1);
+    queue_push(q, (void *)2);
+    queue_push(q, (void *)3);
+
+    pthread_t tids[7];
+    for (int i = 0; i < 7; ++i) { // 2pushes, 5 pulls
+     if (i == 1 || i == 2) 
+       pthread_create(&tids[i], NULL, push, (void *) q);
+     else 
+       pthread_create(&tids[i], NULL, pull, (void *) q);
+    }
+
+    for (int i = 0; i < 7; ++i) 
+        pthread_join(tids[i], NULL);
+
+    queue_destroy(q);
+}
+
+void dont_block_push_neg_max_size() {
+    queue *q = queue_create(-1);
+    queue_push(q, (void *)1);
+    queue_push(q, (void *)2);
+    queue_push(q, (void *)3);
+
+
+    printf("q.top() = %ld\n", (size_t) queue_pull(q)); // try to pull before pushing
+   
+    queue_destroy(q);
+}
+
 int main(int argc, char **argv) {
-    if (argc != 3) {
+    /* if (argc != 3) {
         printf("usage: %s test_number return_code\n", argv[0]);
         exit(1);
     }
-    printf("Please write tests cases\n");
+    printf("Please write tests cases\n"); */
+
+    t1();
+    printf("--------------\n");
+    block_pull_empty();
+    printf("--------------\n");
+    block_push_max_size(); 
+    printf("--------------\n");
+    dont_block_push_neg_max_size();
+    printf("--------------\n");
+
+
+
     return 0;
 }
