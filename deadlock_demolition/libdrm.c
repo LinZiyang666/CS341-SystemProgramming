b16cdf1dd22a7ca7b0d1d0bae0cd15382b366cc6
Vlad Petru Nitu <vnitu2@illinois.edu>
2023-10-04 22:37:35 -0500

    Document both code & tests

--- deadlock_demolition/libdrm.c
+++ deadlock_demolition/libdrm.c
@@ -8,41 +8,49 @@
 #include <bits/pthreadtypes.h>
 #include <pthread.h>
 
+// Protects `graph *g` critical sections
 static pthread_mutex_t m_global = PTHREAD_MUTEX_INITIALIZER; // :warning: The provided graph data structure is not thread-safe.; this ensures thread safeness
-graph *g = NULL;
+graph *g = NULL; 
 struct drm_t
 {
     pthread_mutex_t m;
 };
-set *visited = NULL;
+set *visited = NULL; // used in DFS (has_cycle) to detect cycles; gets reassigned to NULL after running the DFS
 
-int has_cycle(graph *g, void *node) { // run DFS
-  if (!visited) // lazy init
-    visited = shallow_set_create();  
-  
-  if (set_contains(visited, node)) return 1;
-  else {
 
-    int was_visited = false;
-    set_add(visited, node);
-    vector *neigh_vector = graph_neighbors(g, node);
+/*
+Checks if there exists a cycle in the Directed Graph `g` that starts and ends in `node`
+return: 1 (true), if cycle exists, 0 else
+*/
+int has_cycle(graph *g, void *node)
+{                 // run DFS
+    if (!visited) // lazy init
+        visited = shallow_set_create();
 
-    VECTOR_FOR_EACH(neigh_vector, neigh, {
-        if (!set_contains(visited, neigh)) was_visited |= (has_cycle(g, neigh));
-    });
+    if (set_contains(visited, node))
+        return 1;
+    else
+    {
 
-    return was_visited;
-  }
+        int was_visited = false;
+        set_add(visited, node);
+        vector *neigh_vector = graph_neighbors(g, node);
 
+        VECTOR_FOR_EACH(neigh_vector, neigh, { // see vector's MACROs :D
+            if (!set_contains(visited, neigh))
+                was_visited |= (has_cycle(g, neigh)); 
+        }); 
+
+        return was_visited; // if at least one node `has_cycle` => return 1(true), else return 0
+    }
 }
 
 drm_t *drm_init()
 {
-    /* Your code here */
     drm_t *drm = (drm_t *)malloc(sizeof(drm_t));
     pthread_mutex_init(&drm->m, NULL);
 
-  
+    // Critical section for `graph *g` DS
     pthread_mutex_lock(&m_global);
     if (!g) // lazy init
         g = shallow_graph_create();
@@ -54,21 +62,22 @@ drm_t *drm_init()
 
 int drm_post(drm_t *drm, pthread_t *thread_id)
 {
-    /* Your code here */
-
     pthread_mutex_lock(&m_global);
     int exit_code = 0;
-    if (!graph_contains_vertex(g, drm) || !graph_contains_vertex(g, thread_id))
+
+    if (!graph_contains_vertex(g, drm) || !graph_contains_vertex(g, thread_id)) //  both `drm` and `thread_id` should be vertexes that already exist
         exit_code = 0;
     else
     {
 
-        if (graph_adjacent(g, drm, thread_id)) { // if drm -> thred edge exists in graph
-          graph_remove_edge(g, drm, thread_id);  // remove edge
-          pthread_mutex_unlock(&drm->m); // unlock drm
-        }
-        exit_code = 1;
-    }
+        if (graph_adjacent(g, drm, thread_id))
+        {                                         // if (drm -> thread_id) edge exists in graph
+            graph_remove_edge(g, drm, thread_id); // remove this edge
+            pthread_mutex_unlock(&drm->m);        // unlock `drm` (binary semaphore)
+        } 
+
+        exit_code = 1; //TODO: ask in LAB what should we return if drm -> thread_id edge does not exist; I assume 1
+    } 
 
     pthread_mutex_unlock(&m_global);
 
@@ -80,34 +89,32 @@ int drm_wait(drm_t *drm, pthread_t *thread_id)
     pthread_mutex_lock(&m_global);
     int exit_code = 0;
 
-    if (!graph_contains_vertex(g, thread_id)) { // Add the thread to the Resource Allocation Graph if not already present. Hint: what unique identifier can you use for each thread?
+    if (!graph_contains_vertex(g, thread_id))
+    { // Add the thread to the Resource Allocation Graph if not already present. Hint: what unique identifier can you use for each thread?
         graph_add_vertex(g, thread_id);
     }
 
-    if (graph_adjacent(g, drm, thread_id)) { // thread trying to lock a mutex it already owns
+    if (graph_adjacent(g, drm, thread_id))
+    { // thread trying to lock a mutex it already owns => adding (thread_id -> drm) would have caused a cycle in RAG: thread_id -> drm -> thread_id, so DEADLOCK
         pthread_mutex_unlock(&m_global);
         return 0;
-    } 
-
-    graph_add_edge(g, thread_id, drm); // Add the appropriate edge to the Resource Allocation Graph. - attempt to lock
-
-    // int thread_already_owns_mutex = 0;
-    // vector *thread_neigh = graph_neighbors(g, thread_id);
-
-    if (has_cycle(g, thread_id)) // the attempt to lock the drm by the thread would cause deadlock.
-    {
-       graph_remove_edge(g, thread_id, drm); // Add the appropriate edge to the Resource Allocation Graph. - attempt to lock
-       exit_code = 0; 
     }
-    else {
 
+    graph_add_edge(g, thread_id, drm); // Add the appropriate edge to the Resource Allocation Graph - attempt to lock (request resource)
 
-        pthread_mutex_unlock(&m_global); // let `post` to unlock the `drm`, otherwise we deadlock: wait waits for drm->m, post waits for m_global
-        pthread_mutex_lock(&drm->m); // lock the drm; 
+    if (has_cycle(g, thread_id)) // Out attempt to lock the `drm` by the thread would cause deadlock. (cycle in RAG)
+    {
+        graph_remove_edge(g, thread_id, drm); // Remove (thread_id -> drm) edge
+        exit_code = 0;
+    }
+    else
+    {
+        pthread_mutex_unlock(&m_global); // let `post` to unlock the `drm`, otherwise (PREV. WRONG IMPL., see commit history) we deadlock: `drm_wait` waits for `drm->m` held by `drm_post`, `drm_post` waits for `m_global` held by `drm_wait`
+        pthread_mutex_lock(&drm->m);     // lock the `drm`;
+
         pthread_mutex_lock(&m_global);
-
-        graph_remove_edge(g, thread_id, drm);
-        graph_add_edge(g, drm, thread_id); //  DRM is locked by one thread at a time <=> drm has only one outgoing edge at a time
+        graph_remove_edge(g, thread_id, drm); // Our attempt did not create a deadlock => aquire resource
+        graph_add_edge(g, drm, thread_id); //  `drm` owns/uses `thread_id`; `drm `is locked by one thread at a time <=> `drm` has only one outgoing edge at a time
         exit_code = 1;
     }
 
@@ -118,7 +125,6 @@ int drm_wait(drm_t *drm, pthread_t *thread_id)
 
 void drm_destroy(drm_t *drm)
 {
-    /* Your code here */
     if (!drm)
         return;
     graph_remove_vertex(g, drm);
--- deadlock_demolition/libdrm_tester.c
+++ deadlock_demolition/libdrm_tester.c
@@ -15,63 +15,83 @@ int test1 = 0;
 int test2 = 0;
 drm_t *drm1 = NULL;
 drm_t *drm2 = NULL;
-size_t num_threads = 2;
-pthread_mutex_t m1 = PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t m2 = PTHREAD_MUTEX_INITIALIZER;
+const size_t num_threads = 2;
+static pthread_mutex_t m1 = PTHREAD_MUTEX_INITIALIZER;
+static pthread_mutex_t m2 = PTHREAD_MUTEX_INITIALIZER;
 
-void *myfunc1(void *tid) {
-  if (drm_wait(drm1, tid))
-    test1++;
-  else printf("deadlock 1!!!\n");
-  sleep(1);
+void *myfunc1(void *tid)
+{
+    if (drm_wait(drm1, tid)) // d1 succeeds in locking tid1 => tid1 acquired d1 (d1 -> t1)
+        test1++;
+    else
+        printf("deadlock 1!!!\n");
+    sleep(1);
 
-  if (drm_wait(drm2, tid)) {
-    for (int i = 0; i < 10000; i++) {
-      test2++;
+    if (drm_wait(drm2, tid))
+    { // d2 tries and does not succeed in locking tid1 => d2 asks for t1 (t1 -> d2)
+        for (int i = 0; i < 1000; i++)
+        { // use diff. counters to see that we don't run into this loop
+            test2++;
+        }
     }
-  } else printf("deadlock 2!!!\n");
+    else
+        printf("deadlock 2!!!\n"); // A deadlock here should be created: drm1 -> tid1 -> drm2 -> tid2
 
-  if (drm_post(drm2, tid) == 0)
-    printf("error!!!\n");
+    if (drm_post(drm2, tid) == 0) // d2 will let tid1 go
+        printf("error!!!\n");
 
-  if (drm_post(drm1, tid) == 0)
-    printf("error!!!\n");
-  return NULL;
+    if (drm_post(drm1, tid) == 0) // d1 will let tid1 go
+        printf("error!!!\n");
+    return NULL;
 }
-void *myfunc2(void *tid) {
-  if (drm_wait(drm2, tid)) {
-    for (int i = 0; i < 1000000; i++) {
-      test2++;
+void *myfunc2(void *tid)
+{
+    if (drm_wait(drm2, tid))
+    { // d2 tries and succeeds to lock tid2 => (d2 -> t2)
+        for (int i = 0; i < 10; i++)
+        {
+            test2++;
+        }
     }
-  } else printf("deadlock 3!!!\n");
+    else
+        printf("deadlock 3!!!\n");
 
-  if (drm_wait(drm1, tid))
-    test1++;
-  else printf("deadlock 4!!!\n");
+    if (drm_wait(drm1, tid)) // d1 tries to lock tid2, does not suceed => (t2 -> d1)
+        test1++;
+    else
+        printf("deadlock 4!!!\n");
 
-  if (drm_post(drm1, tid) == 0)
-    printf("error!!!\n");
+    if (drm_post(drm1, tid) == 0) // d1 will let tid2 go
+        printf("error!!!\n");
 
-  if (drm_post(drm2, tid) == 0)
-    printf("error!!!\n");
-  return NULL;
+    if (drm_post(drm2, tid) == 0) // d2 will let tid2 go
+        printf("error!!!\n");
+    return NULL;
 }
-int main() {
+int main()
+{
     drm1 = drm_init();
     drm2 = drm_init();
     pthread_t tid[num_threads];
-    pthread_t *tid_ptr1 = tid; 
-    pthread_t *tid_ptr2 = tid + 1; 
+    pthread_t *tid_ptr1 = tid;
+    pthread_t *tid_ptr2 = tid + 1;
     pthread_create(tid, NULL, myfunc1, tid_ptr1);
-    pthread_create(tid+1, NULL, myfunc2, tid_ptr2);
+    pthread_create(tid + 1, NULL, myfunc2, tid_ptr2);
     pthread_join(tid[0], NULL);
     pthread_join(tid[1], NULL);
 
-    printf("test1: %d\n", test1);
-    printf("test2: %d\n", test2);
+    // We expect deadlock: RAG (Resource Allocation Graph): d1 -> t1  -> d2 -> r2 -> d1  has a cycle
+
+    printf("test1: %d\n", test1); // exp. 2
+    printf("test2: %d\n", test2); // exp. 10
     drm_destroy(drm1);
     drm_destroy(drm2);
     pthread_mutex_destroy(&m1);
     pthread_mutex_destroy(&m2);
     pthread_exit(NULL);
+
+    // Expected output (program's behavior)
+    /* deadlock 2!!!
+test1: 2
+test2: 10 */
 }
