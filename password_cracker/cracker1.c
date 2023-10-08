88569933ab777b1642b96cd9bf197641aa929d23
Vlad Petru Nitu <vnitu2@illinois.edu>
2023-10-08 12:09:22 -0500

    Define structs & form of the program, still have to impl sovle_task for Part1

--- password_cracker/cracker1.c
+++ password_cracker/cracker1.c
@@ -5,10 +5,93 @@
 #include "cracker1.h"
 #include "format.h"
 #include "utils.h"
+#include <crypt.h>
+#include <pthread.h>
+#include "includes/queue.h"
+#include <stdio.h>
+#include <string.h>
+
+
+#define UNBOUNDED (-1)
+#define TRUE 1
+#define FALSE 0
+#define SENTINEL_Q NULL
+
+// maude xxEe0WApyDMcg a......
+struct task {
+    char *username; 
+    char *hashed_pwd; 
+    char *known_pwd; // contains `...` at the end
+};
+
+typedef struct task task_t;
+
+static pthread_mutex_t numRecoveredMtx= PTHREAD_MUTEX_INITIALIZER; // successful password cracks
+static pthread_mutex_t numFailedMtx = PTHREAD_MUTEX_INITIALIZER; // unsuccessful (failed) password cracks
+
+static int numRecovered = 0;
+static int numFailed = 0;
+
+queue *Q = NULL;
+
+
+void parse_line(char *lineptr, char **username, char **hashed_pwd, char **known_pwd) {
+    *username = strtok(lineptr, " ");
+    *hashed_pwd = strtok(lineptr, " ");
+    *known_pwd = strtok(lineptr, " ");
+}
+
+task_t *create_task(char *username, char *hashed_pwd, char *known_pwd) {
+        task_t *tsk = malloc(sizeof(task_t));
+        tsk->username = strdup(username);
+        tsk->hashed_pwd = strdup(hashed_pwd);
+        tsk->known_pwd = strdup(known_pwd);
+        return tsk;
+}
+
+
 
 int start(size_t thread_count) {
     // TODO your code here, make sure to use thread_count!
     // Remember to ONLY crack passwords in other threads
+
+    Q = queue_create(UNBOUNDED);
+    char *lineptr = NULL;
+    size_t n = 0;
+    size_t nbytes = 0;
+
+    while ((nbytes = getline(&lineptr, n, stdin))) { // read from STDIN 
+        
+        if (nbytes == -1) break; 
+        if (nbytes > 0 && lineptr[nbytes - 1] == '\n') // remove '\n' appended by getline
+            lineptr[nbytes - 1] == '\0';
+
+        char *username = NULL;
+        char *hashed_pwd = NULL;
+        char *known_pwd = NULL;
+        parse_line(lineptr, &username, &hashed_pwd, &known_pwd);
+
+        task_t *tsk = create_task(username, hashed_pwd, known_pwd);
+
+        queue_push(Q, tsk);
+    }
+    free(lineptr);
+
+    queue_push(Q, SENTINEL_Q); // the analogue of the null byte)
+
+    // Create `thread_count` threads
+    pthread_t tids[thread_count];
+    for (int i = 0; i < thread_count; ++i) {
+        pthread_create(tids + i, NULL, solve_task, (void *)(i + 1)); // tids are 1-indexed !
+        if (i + 1 == thread_count) // last thread has been created
+            for (int j = 0; j < thread_count; ++j) //  Every thread must join with the main thread!
+                pthread_join(tids[i], NULL);
+    }
+
+
+    v1_print_summary(numRecovered, numFailed);
+    queue_destroy(Q);
+
     return 0; // DO NOT change the return code since AG uses it to check if your
               // program exited normally
 }
