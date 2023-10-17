d6161c56bf5349767885538bb143afac1a44399f
Vlad Petru Nitu <vnitu2@illinois.edu>
2023-10-17 17:19:04 -0500

    Added run_status check before solving threads

--- parallel_make/parmake.c
+++ parallel_make/parmake.c
@@ -7,16 +7,11 @@
 #include "graph.h"
 #include "parmake.h"
 #include "parser.h"
-
-int has_cycle(void *target);
-int tricolor(dictionary *mp, void *target);
-
-void get_rules_in_order(vector *targets);
-void get_rules(dictionary *cnt, vector *targets);
-
-
-// Method that is called when thread is created
-void* solve(void *arg); 
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <time.h>
 
 // The graph and vector classes are not thread-safe! ->
 // a.) rule_lock, rule_cv for `rules`
@@ -28,29 +23,130 @@ pthread_cond_t g_cv = PTHREAD_COND_INITIALIZER;
 pthread_mutex_t rule_lock = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t g_lock = PTHREAD_MUTEX_INITIALIZER;
 
-
 /* Helper Functions */
 
 // Allocates a string -> int dictionary and returns it
-dictionary *dictionary_init() {
+dictionary *dictionary_init()
+{
     dictionary *mp = string_to_int_dictionary_create();
     vector *nodes = graph_vertices(g);
     size_t num_nodes = vector_size(nodes);
-    for (size_t i = 0; i < num_nodes; ++i) {
-        int val = 0; 
+    for (size_t i = 0; i < num_nodes; ++i)
+    {
+        int val = 0;
         dictionary_set(mp, vector_get(nodes, i), &val);
     }
+    vector_destroy(nodes); // Any vectors returned from graph functions must be destroyed manually to prevent memory leaks. Destroying these vectors will not destroy anything in the actual graph.
     return mp;
 }
 
-// ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ // 
+int has_cycle(void *target);
+int tricolor(dictionary *mp, void *target);
+
+void get_rules_in_order(vector *targets);
+void get_rules(dictionary *cnt, vector *targets);
+
+// Method that is called when thread is created
+void *solve(void *arg);
+
+/* Return an integer depicitng the status of running the current rule
+  -1: error, don't run, mark failed
+  0: dependencies not finished yet
+  1: all deps are satisfied, run IMMEDIATELY
+  2: should not run, don't mark as satisfied
+  3: rule is already done previously
+*/
+int run_status(void *target)
+{
+    rule_t *rule = (rule_t *)graph_get_vertex_value(g, target); // access MakeFile rule from graph
+
+    if (rule->state != 0)                                       // already done previously
+        return 3;                                               //  Once a rule is satisfied, it shouldn’t be run again.
+
+    vector *dependencies = graph_neighbors(g, target);
+    size_t num_dependencies = vector_size(dependencies);
+    if (num_dependencies > 0)
+    {
+        int is_file_1 = (access((char *)target, F_OK) != -1);
+        if (is_file_1)
+        { // The rule is the name of a file on disk
+            for (size_t i = 0; i < num_dependencies; ++i)
+            {
+                char *dependency = vector_get(dependencies, i);
+                int is_file_2 = (access(dependency, F_OK) != -1);
+                if (is_file_2)
+                { //  it depends on another file
+
+                    struct stat stat_file_1, stat_file_2;
+                    int err_stat_1 = stat(target, &stat_file_1);
+                    int err_stat_2 = stat(target, &stat_file_2);
+                    if (err_stat_1 == -1 || err_stat_2 == -1) // stat failed
+                    {
+                        vector_destroy(dependencies);
+                        return -1;
+                    }
+                    // Granularity of 1s
+                    int diff = difftime(stat_file_1.st_mtime, stat_file_2.st_mtime);
+                    if (diff < 0) // dependency is NEWER
+                    {
+                        vector_destroy(dependencies);
+                        return 1;
+                    }
+                }
+                else
+                { // The rule depends on another rule that is not the name of a file on disk
+                    vector_destroy(dependencies);
+                    return 1;
+                }
+            }
+
+            vector_destroy(dependencies);
+            return 2; //don't run TODO: ask lab
+        } 
+        else { // `target` not a file -> check if all dependencies succeeded (for any dependency: dependency->state = 1), if true -> 1,
+        //  else propagate dependency's state by returning it: 0: if dependency not met yet, -1: if dependency had error
+        
+            pthread_mutex_lock(&g_lock); // graph class is not thread safe -> lock mutex to access its members
+            // We now have to lock the mutex, as we access dep->state, to ensure that no other thread modifies it while we read it
+
+            for (size_t i = 0; i < num_dependencies; ++i) {
+                char *dependency = vector_get(dependencies, i);
+                rule_t *sub_rule = (rule_t *)graph_get_vertex_value(g, dependency);
+                if (sub_rule->state != 1) // dependency not succeeded yet
+                {
+                    pthread_mutex_unlock(&g_lock);
+                    vector_destroy(dependencies);
+                    return sub_rule->state;
+                }
+            }
+
+            // All subrules / dependencies suceeded :D  => run current rule immediately
+            pthread_mutex_unlock(&g_lock);
+            vector_destroy(dependencies);
+            return 1;
+        }
+    }
+    else
+    { // rule does not depend on any other rule
+        vector_destroy(dependencies);
+        int is_file_1 = (access((char *)target, F_OK) != -1);
+        if (!is_file_1)
+            return 1;
+        else
+            return 2;
+    }
+}
+
+
+// ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ //
 // Cycle methods
 
 // 1, if graph has cycle starting traversal from `target`, else 0
-int has_cycle(void *target) {
+int has_cycle(void *target)
+{
     dictionary *mp = dictionary_init();
-    int isCycle = tricolor(mp, target); 
-    dictionary_destroy(mp); //TODO: LAB - Ask if this hsould be destroyed.
+    int isCycle = tricolor(mp, target);
+    dictionary_destroy(mp); // TODO: LAB - Ask if this hsould be destroyed.
     return isCycle;
 }
 
@@ -58,33 +154,36 @@ int has_cycle(void *target) {
 Source: https://www.cs.cornell.edu/courses/cs2112/2012sp/lectures/lec24/lec24-12sp.html
 Performs Tricolor algorithm to detect cycle
 Each entry in the `mp` dictionary is in one of the following 3 states:
-mp[key] = 0 -> (White) Not visited yet 
+mp[key] = 0 -> (White) Not visited yet
 mp[key] = 1 -> (Grey) in progress
 mp[key] = 2 -> (Black) finished
 Returns: 1, if cycle found, else 0
 */
-int tricolor (dictionary *mp, void *target) {
+int tricolor(dictionary *mp, void *target)
+{
 
     int *value = (int *)dictionary_get(mp, target);
     if (*value == 1) // got to a Grey node once again => CYCLE
-        return 1; 
+        return 1;
     else if (*value == 2) // `target` is Black -> done, no cycle
-        return 0; 
-    
+        return 0;
+
     // mark as `in progress` (Gray)
     dictionary_set(mp, target, 1);
-    vector *children = graph_neighbors(g, target); 
+    vector *children = graph_neighbors(g, target);
     size_t num_children = vector_size(children);
 
-    for (size_t i = 0; i < num_children; ++i) {
+    for (size_t i = 0; i < num_children; ++i)
+    {
         int isCycle = tricolor(mp, vector_get(children, i));
-        if (isCycle) {
+        if (isCycle)
+        {
             vector_destroy(children);
             return isCycle;
         }
     }
-    
-    //mark as `finished` (Black)
+
+    // mark as `finished` (Black)
     dictionary_set(mp, target, 2);
     vector_destroy(children);
     return 0; // IF NONE of children produced cycle -> no cycle
@@ -94,11 +193,12 @@ int tricolor (dictionary *mp, void *target) {
 // Gray (1) -> have been discovered but that the algorithm is not done with yet; Frontier between White and Black
 // Black (2) -> node that algorithm is done with.
 
-// ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ // 
+// ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ //
 // Rules methods
 
-void get_rules_in_order(vector *targets) {
-    dictionary *cnt = dictionary_init();
+void get_rules_in_order(vector *targets)
+{
+    dictionary *cnt = dictionary_init(); // TODO: Later change to use a SET
     get_rules(cnt, targets);
     dictionary_destroy(cnt);
 }
@@ -107,27 +207,26 @@ void get_rules_in_order(vector *targets) {
 // Each entry in `cnt` dictionary can be in one of the following states:
 // 0 -> !visited yet
 // 1 -> visited
-void get_rules (dictionary *cnt, vector *targets) {
+void get_rules(dictionary *cnt, vector *targets)
+{
     size_t num_targets = vector_size(targets);
-    for (size_t i = 0; i < num_targets; ++i) {
+    for (size_t i = 0; i < num_targets; ++i)
+    {
         void *target = vector_get(targets, i);
         void *dependencies = graph_neighbors(g, target);
         get_rules(cnt, dependencies); // DFS
 
-        int seen = (*(int*)dictionary_get(g, target));
-        if (!seen) {
-            dictionary_set(cnt, target, 1); // mark as seen
+        int seen = (*(int *)dictionary_get(g, target));
+        if (!seen)
+        {
+            dictionary_set(cnt, target, 1);  // mark as seen
             vector_push_back(rules, target); // enqueue
         }
         vector_destroy(dependencies);
     }
 }
 
-// ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ // 
-
-
-
-
+// ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ //
 
 int parmake(char *makefile, size_t num_threads, char **targets)
 {
@@ -136,28 +235,32 @@ int parmake(char *makefile, size_t num_threads, char **targets)
     vector *goal_rules = graph_neighbors(g, "");         // only work on rules that descend from this rule (i.e. the goal rules and all their descendants)
     size_t ngoals = vector_size(goal_rules);
 
-    int isCycle = 0; 
-    for (size_t i = 0; i < ngoals; ++i) {
-        char* goal_rule = vector_get(goal_rules, i);
-        if (has_cycle(goal_rule)) {
+    int isCycle = 0;
+    for (size_t i = 0; i < ngoals; ++i)
+    {
+        char *goal_rule = vector_get(goal_rules, i);
+        if (has_cycle(goal_rule))
+        {
             print_cycle_failure((char *)goal_rule);
             isCycle = 1;
         }
     }
 
-    if (isCycle) {
+    if (isCycle)
+    {
         vector_destroy(goal_rules);
         graph_destroy(g);
         return 0;
     }
-    else {
+    else
+    {
         rules = shallow_vector_create(); // to allow caller (ME) to perform memory management
-        get_rules_in_order(goal_rules); //TODO: The rule’s commands need to be run sequentially in the order they appear in the command vector
+        get_rules_in_order(goal_rules);  // The rule’s commands need to be run sequentially in the order they appear in the command vector
 
         pthread_t threads[num_threads];
-        for (int i = 0; i < num_threads; ++i) 
+        for (int i = 0; i < num_threads; ++i)
             pthread_create(threads + i, NULL, solve, NULL);
-        for (int i = 0; i < num_threads; ++i) 
+        for (int i = 0; i < num_threads; ++i)
             pthread_join(threads[i], NULL);
 
         vector_destroy(rules);
