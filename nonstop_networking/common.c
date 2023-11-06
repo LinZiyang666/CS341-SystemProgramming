d53b889eb9227fab1dd38d96eae698eae8ca31ef
Vlad Petru Nitu <vnitu2@illinois.edu>
2023-11-05 20:40:25 -0600

    Set up common file w/ boilerplate read/write/error code

--- nonstop_networking/common.c
+++ nonstop_networking/common.c
@@ -3,3 +3,71 @@
  * CS 341 - Fall 2023
  */
 #include "common.h"
+#include "format.h"
+#include <errno.h>
+
+#define MAX_HEADER_LEN 1024
+
+ssize_t read_from_socket(int socket, char *buffer, size_t count) {
+    // Your Code Here
+    size_t bytes_read = 0;
+    while (bytes_read < count)
+    { // have not read all bytes
+        int remaining_bytes_to_read = count - bytes_read;
+        ssize_t newly_read_bytes = read(socket, buffer + bytes_read, remaining_bytes_to_read);
+        if (newly_read_bytes == 0) // socket disconnected
+            break;
+        else if (newly_read_bytes == -1)
+        { // error
+            // check error code
+            if (errno == EINTR) // interrupted
+                continue;       // retry
+            else
+                return -1; // actual error
+        }
+
+        bytes_read += newly_read_bytes; 
+    }
+    return bytes_read;
+}
+
+ssize_t write_to_socket(int socket, const char *buffer, size_t count)  {
+    size_t bytes_wrote = 0;
+    while (bytes_wrote < count)
+    { // have not written all bytes
+        int remaining_bytes_to_write = count - bytes_wrote;
+        ssize_t newly_wrote_bytes = write(socket, (char *) buffer + bytes_wrote, remaining_bytes_to_write);
+        if (newly_wrote_bytes == 0) // socket disconnected
+            break;
+        else if (newly_wrote_bytes == -1)
+        { // error
+            // check error code
+            if (errno == EINTR) // interrupted
+                continue;       // retry
+            else
+                return -1; // actual error
+        }
+
+        bytes_wrote += newly_wrote_bytes;
+    }
+    return bytes_wrote;
+}
+
+
+int is_error(size_t read_bytes, size_t buffer_size) {
+    if (read_bytes == 0 && read_bytes != buffer_size) // connection got closed
+     {  
+        print_connection_closed();
+        return 1;
+     }
+     else if (read_bytes < buffer_size) {
+        print_too_little_data();
+        return 1;
+     }
+     else if (read_bytes > buffer_size) {
+        print_received_too_much_data();
+        return 1;
+     }
+     
+     return 0; // No error :D
+}
--- nonstop_networking/common.h
+++ nonstop_networking/common.h
@@ -5,6 +5,8 @@
 #pragma once
 #include <stddef.h>
 #include <sys/types.h>
+#include "format.h"
+#include <errno.h>
 
 #define LOG(...)                      \
     do {                              \
@@ -13,3 +15,18 @@
     } while (0);
 
 typedef enum { GET, PUT, DELETE, LIST, V_UNKNOWN } verb;
+
+
+// `read_from_socket` and `write_to_socket` taken from `charming_chatroom` lab assignment impl.
+
+// Reads `count` bytes from `socket` into the `buffer` 
+// RET: `bytes_read`: the number of bytes succesfully read from the buffer, or -1 in case of an error
+ssize_t read_from_socket(int socket, char *buffer, size_t count);
+
+// Write `count` bytes from `buffer` into the `socket` 
+// RET: `wrote_bytes`: the number of bytes succesfully wrote into the buffer, or -1 in case of an error
+ssize_t write_to_socket(int socket, const char *buffer, size_t count);
+
+// RET: 1, if an error is detected (and calls `print_connection_closed`, `print_too_little_data`, or `print_received_too_much_data`), depending on the error
+// or 0, if no error was found
+int is_error(size_t sz1, size_t sz2);
